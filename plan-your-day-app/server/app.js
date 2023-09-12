8c975d2eecdcd93bedf8ef0883e0dd565723b8b7
Nahiyan-16 <71025942+Nahiyan-16@users.noreply.github.com>
2023-09-11 22:39:01 -0400

    First working server with password protection

--- plan-your-day-app/server/app.js
+++ plan-your-day-app/server/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path'); // For serving static files
 const User = require('./models/User'); // Import the User model
+const bcrypt = require('bcrypt');
 
 const app = express();
 const port = process.env.PORT || 3001; // Backend server port
@@ -30,35 +31,61 @@ db.once('open', () => {
 
 // API routes (backend)
 app.post('/login', async (req, res) => {
-    try {
-      const { email, password } = req.body;
-  
-      // Check if the user with the given email exists in the database
-      const user = await User.findOne({ email });
+  try {
+    const { email, password } = req.body;
 
-      // If no user with the email is found, return an error
-      if (!user) {
-        return res.status(401).json({ error: 'Invalid email or password' });
-      }
-  
-      // Check if the provided password matches the user's stored password
-      const passwordMatch = true/* Add your password comparison logic here */;
-  
-      if (!passwordMatch) {
-        return res.status(401).json({ error: 'Invalid email or password' });
-      }
-  
-      // At this point, the email and password are valid, and the user is authenticated.
-      // You can generate a token for the user, set up a session, or perform any other
-      // authentication actions here.
-      
-      // For demonstration purposes, you can send a success message
-      res.status(200).json({ message: 'Login successful!' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'An error occurred while processing your request' });
+    // Check if the user with the given email exists in the database
+    const user = await User.findOne({ email });
+
+    // If no user with the email is found, return an error
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid email or password' });
     }
-  });
+
+    // Check if the provided password matches the user's stored password
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+
+    // At this point, the email and password are valid, and the user is authenticated.
+    // You can generate a token for the user, set up a session, or perform any other
+    // authentication actions here.
+    
+    // For demonstration purposes, you can send a success message
+    res.status(200).json({ message: 'Login successful!' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while processing your request' });
+  }
+});
+
+app.post('/signup', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if the email is already in use
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ error: 'Email already in use' });
+    }
+
+    // Hash the password (using bcrypt or your preferred library)
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user document
+    const newUser = new User({ email, password: hashedPassword });
+
+    await newUser.save();
+
+    res.status(201).json({ message: 'Registration successful' });
+  } catch (error) {
+    console.error('Registration error:', error);
+    res.status(500).json({ error: 'An error occurred during registration' });
+  }
+});
 
 // Serve the React frontend (build) as a static asset
 app.use(express.static(path.join(__dirname, '../build')));
--- plan-your-day-app/src/Components/SignUpPage/signUpPage.js
+++ plan-your-day-app/src/Components/SignUpPage/signUpPage.js
@@ -2,25 +2,41 @@ import { useState } from 'react';
 import SideHeader from '../Header/sideHeader';
 
 function SignUpPage(){
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
-        confirmPassword: '',
-    });
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value,
-        });
-    };
-
-    const handleSubmit = (e) => {
-    e.preventDefault();
-    // Add registration logic here (to be implemented in the backend).
-    // Send formData to the server for registration.
-    };
+    const handleRegistration = async (e) => {
+        e.preventDefault();
+        if(confirmPassword === password){
+            try {
+              // Send registration data to the server
+              const response = await fetch('/signup', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email, password }),
+              });
+        
+              if (response.ok) {
+                // Registration successful
+                // Redirect to a success page or login page
+                window.location.href='/home'
+              } else {
+                const data = await response.json();
+                setErrorMessage(data.error);
+              }
+            } catch (error) {
+              console.error('Sign Up error:', error);
+              // Handle other errors, e.g., network issues
+            } 
+        }
+        else {
+            setErrorMessage('Passwords do not match')
+        }
+      };
 
     return (
         <div className="h-screen text-center flex justify-start xs:flex-col">
@@ -28,16 +44,16 @@ function SignUpPage(){
             <div className="flex flex-col justify-center align-center bg-transparent text-charcoal w-[30%] h-screen
              shadow-[inset_0_0_15px_5px_rgba(0,0,0,0.5)] custom-screen-3:w-[100%] custom-screen-2:w-[50%]">
                 <h2 className='mb-10 text-5xl custom-screen-1:text-3xl'>Registration</h2>
-                <form onSubmit={handleSubmit} className="self-center">
+                <form onSubmit={handleRegistration} className="self-center">
                     <div className="text-left flex flex-col align-center mt-2.5 mb-1.5 border-b border-solid border-ice">
                         <label className="mb-1.5 text-[1.25rem] self-start">Email</label>
                         <input
-                            className="textshadow bg-transparent text-aliceblue p-2.5 border-none text-opacity-8 text-[.80rem]"
+                            className="bg-transparent p-2.5 border-none text-opacity-8 text-[.80rem]"
                             type="email"
                             name="email"
                             placeholder='Type your email'
-                            value={formData.email}
-                            onChange={handleChange}
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                             autoFocus
                             required
                         />
@@ -45,29 +61,31 @@ function SignUpPage(){
                     <div className="text-left flex flex-col align-center mt-2.5 mb-1.5 border-b border-solid border-ice">
                         <label className="mb-1.5 text-[1.25rem] self-start">Password</label>
                         <input
-                            className="textshadow bg-transparent text-aliceblue p-2.5 border-none text-opacity-8 text-[.80rem]"
+                            className="bg-transparent p-2.5 border-none text-opacity-8 text-[.80rem]"
                             type="password"
                             name="password"
                             placeholder='Enter your password'
-                            value={formData.password}
-                            onChange={handleChange}
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                             required
                         />
                     </div>
                     <div className="text-left flex flex-col align-center mt-2.5 mb-10 border-b border-solid border-ice">
                         <label className="mb-1.5 text-[1.25rem] self-start">Confirm Password</label>
                         <input                        
-                            className="textshadow bg-transparent text-aliceblue p-2.5 border-none text-opacity-8 text-[.80rem]"
+                            className="bg-transparent p-2.5 border-none text-opacity-8 text-[.80rem]"
                             type="password"
                             name="confirmPassword"
                             placeholder='Re-Enter your password'
-                            value={formData.confirmPassword}
-                            onChange={handleChange}
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
                             required
                         />
                     </div>
                     <div>
-                        <button className="text-aliceblue text-xl shadow-[2px_2px_2px_black] transition-all duration-[250ms] m-2.5 px-5 py-2.5 rounded-[10px] border-[none] hover:cursor-pointer hover:shadow-[4px_4px_4px_black] bg-charcoal" type="submit">Register</button>
+                        <button className="text-aliceblue text-xl shadow-[2px_2px_2px_black] transition-all duration-[250ms] 
+                        m-2.5 px-5 py-2.5 rounded-[10px] border-[none] hover:cursor-pointer hover:shadow-[4px_4px_4px_black] 
+                        bg-charcoal" type="submit">Register</button>
                     </div>
                 </form>
             </div>
